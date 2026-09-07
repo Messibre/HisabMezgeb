e5eb64bcd880aa5718d907250ea749240f1e68fc
Messibre <messibre27@gmail.com>
2026-09-07 22:37:25 +0300

    feat(auth): update auth config from local storage to cookies

--- frontend/src/routes/ProtectedRoute.tsx
+++ frontend/src/routes/ProtectedRoute.tsx
@@ -3,6 +3,6 @@ import { useAuthStore } from '@/store/auth.store';
 import { ROUTES } from '@/constants';
 
 export default function ProtectedRoute() {
-  const token = useAuthStore((s) => s.token);
-  return token ? <Outlet /> : <Navigate to={ROUTES.LOGIN} replace />;
+  const user = useAuthStore((s) => s.user);
+  return user ? <Outlet /> : <Navigate to={ROUTES.LOGIN} replace />;
 }
--- frontend/src/routes/PublicRoute.tsx
+++ frontend/src/routes/PublicRoute.tsx
@@ -3,6 +3,6 @@ import { useAuthStore } from '@/store/auth.store';
 import { ROUTES } from '@/constants';
 
 export default function PublicRoute() {
-  const token = useAuthStore((s) => s.token);
-  return token ? <Navigate to={ROUTES.HOME} replace /> : <Outlet />;
+  const user = useAuthStore((s) => s.user);
+  return user ? <Navigate to={ROUTES.HOME} replace /> : <Outlet />;
 }
